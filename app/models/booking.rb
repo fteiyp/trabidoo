d518d29eecf35fdea83daf4f456569975412573b
Ondrej Svejkovsky <ondrej.svejkovsky@gmail.com>
2019-11-18 17:44:27 +0100

    add validation for all models, add column is_active to all tables

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :trabi
+
+  validates :user_id, :trabi_id, :start_date, :end_date, presence: true
+  validates :rating_review, inclusion: { in: (0..5).to_a }, numericality: { only_integer: true }
 end
--- app/models/trabi.rb
+++ app/models/trabi.rb
@@ -1,8 +1,12 @@
 class Trabi < ApplicationRecord
   belongs_to :user
   has_many :pictures
+  has_many :bookings
+
+  validates :user_id, :color, presence: true
+  validates :year, presence: true, inclusion: { in: (1957..1990).to_a }, numericality: { only_integer: true }
   # Nested attributes will allow us to save attributes on associated
   # records through the parent,
   # https://kolosek.com/carrierwave-upload-multiple-images/
-  accepts_nested_attributes_for :post_attachments
+  # accepts_nested_attributes_for :post_attachments
 end
