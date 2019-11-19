6ae5f3b47bc848f0bfe35d74c8fdadb9cf2c4036
Joseph Kunz <joseph.kunz@gmx.de>
2019-11-19 11:08:04 +0100

    added index and show to controller & created and implemented index view

--- app/controllers/trabis_controller.rb
+++ app/controllers/trabis_controller.rb
@@ -1,2 +1,14 @@
 class TrabisController < ApplicationController
+  def index
+    @trabis = Trabi.all
+  end
+
+  def show
+    @trabis = Trabi.find(params[:id])
+  end
+
+  private
+
+  def trabi_params
+  end
 end
