5cd7ed9a82c1c5d0b5df90f6013296aa2ad76b8a
Joseph Kunz <joseph.kunz@gmx.de>
2019-11-19 12:55:14 +0100

    in trabi controller I implemented new, create & params

--- app/controllers/trabis_controller.rb
+++ app/controllers/trabis_controller.rb
@@ -7,8 +7,19 @@ class TrabisController < ApplicationController
     @trabis = Trabi.find(params[:id])
   end
 
+  def new
+    @trabi = Trabi.new
+  end
+
+  def create
+    @trabi = Trabi.new(trabi_params)
+    @trabi.save
+    redirect_to trabi_path(@trabi)
+  end
+
   private
 
   def trabi_params
+    params.require(:trabi).permit(:title, :description)
   end
 end
