8d1c98ebce7033d3acb2c0979efd0c2d043b913b
Ondrej Svejkovsky <ondrej.svejkovsky@gmail.com>
2019-11-20 23:13:25 +0100

    user.js updated

--- app/javascript/packs/user.js
+++ app/javascript/packs/user.js
@@ -8,161 +8,274 @@ const trabisLink = document.getElementById("trabis-link")
 const trabisTab = document.getElementById("trabis-tab")
 
 const dropZoneElement = document.getElementById("files-field");
-const userForm = document.getElementById('user-form');
-const inputFirstName = document.getElementById("input-first-name");
-const inputLastName = document.getElementById("input-last-name");
-const inputEmail = document.getElementById("input-email");
-const inputPhone = document.getElementById("input-phone");
-const inputStreet = document.getElementById("input-street");
-const inputCity = document.getElementById("input-city");
-const inputZip = document.getElementById("input-zip");
-const inputCountry = document.getElementById("input-country");
+const userForm = document.querySelector("#user-info-tab form");//document.getElementById('user-form');
+const inputFirstName = document.getElementById("user_first_name");
+const inputLastName = document.getElementById("user_last_name");
+const inputEmail = document.getElementById("user_email");
+const inputPhone = document.getElementById("user_phone_number");
+const inputStreet = document.getElementById("user_street_address");
+const inputCity = document.getElementById("user_city_address");
+const inputZip = document.getElementById("user_zip_address");
+const inputCountry = document.getElementById("user_country_address");
+const csfrToken = document.querySelector('[name="authenticity_token"]');
+
+console.log(csfrToken);
 
 userInfoLink.classList.add('active', 'bg-white');
 userInfoTab.style.display = 'block';
 bookingTab.style.display = 'none';
 trabisTab.style.display = 'none';
 
-const switchTab = (tab) => {
+userInfoLink.addEventListener("click", (event) => {
+  userInfoTab.style.display = 'block';
+  bookingTab.style.display = 'none';
+  trabisTab.style.display = 'none';
 
-  if (tab  === 'user-info-link') {
-    userInfoTab.style.display = 'block';
-    bookingTab.style.display = 'none';
-    trabisTab.style.display = 'none';
+  userInfoLink.classList.add('active', 'bg-white');
+  bookingLink.classList.remove('active', 'bg-white');
+  trabisLink.classList.remove('active', 'bg-white');
+});
 
-    userInfoLink.classList.add('active', 'bg-white');
-    bookingLink.classList.remove('active', 'bg-white');
-    trabisLink.classList.remove('active', 'bg-white');
-  } else if (tab  === 'bookings-link') {
-    userInfoTab.style.display = 'none';
-    bookingTab.style.display = 'block';
-    trabisTab.style.display = 'none';
+bookingLink.addEventListener("click", (event) => {
+  userInfoTab.style.display = 'none';
+  bookingTab.style.display = 'block';
+  trabisTab.style.display = 'none';
 
-    userInfoLink.classList.remove('active','bg-white');
-    bookingLink.classList.add('active', 'bg-white');
-    trabisLink.classList.remove('active', 'bg-white');
-  } else {
-    userInfoTab.style.display = 'none';
-    bookingTab.style.display = 'none';
-    trabisTab.style.display = 'block';
+  userInfoLink.classList.remove('active','bg-white');
+  bookingLink.classList.add('active', 'bg-white');
+  trabisLink.classList.remove('active', 'bg-white');
+});
 
-    userInfoLink.classList.remove('active', 'bg-white');
-    bookingLink.classList.remove('active', 'bg-white');
-    trabisLink.classList.add('active', 'bg-white');
-  }
+trabisLink.addEventListener("click", (event) => {
+  userInfoTab.style.display = 'none';
+  bookingTab.style.display = 'none';
+  trabisTab.style.display = 'block';
 
+  userInfoLink.classList.remove('active', 'bg-white');
+  bookingLink.classList.remove('active', 'bg-white');
+  trabisLink.classList.add('active', 'bg-white');
+});
+
+const dropZoneOnSuccess = (data,response) => {
+  alert("User info succesfuly updated");
+  window.location.reload();
 };
 
-let url = location.href
+const appendFormData = (formData) => {
 
-const tabClicked = (event) => {
-  let newUrl;
+  formData.append("user[first_name]", inputFirstName.value);
+  formData.append("user[last_name]", inputLastName.value);
+  formData.append("user[email]", inputEmail.value);
+  formData.append("user[phone_number]", inputPhone.value);
+  formData.append("user[street_address]", inputStreet.value);
+  formData.append("user[city_address]", inputCity.value);
+  formData.append("user[zip_address]", inputZip.value);
+  formData.append("user[country_address]", inputCountry.value);
 
-  switchTab(event.target.getAttribute("id"));
-  const hash = event.target.getAttribute("href");
-  let prefix = url.split("#")[0];
-
-  if (prefix[prefix.length - 1] === "/") {
-    prefix = prefix.substring(0, prefix.length - 1)
-  }
-
-  newUrl = prefix + hash;
-
-  history.replaceState(null, null, newUrl);
+  return formData;
 };
 
-userInfoLink.addEventListener("click", tabClicked);
-bookingLink.addEventListener("click", tabClicked);
-trabisLink.addEventListener("click", tabClicked);
+Dropzone.options.filesField = {
+  url: (window.location.pathname),
+  method: 'put',
+  maxFiles: 1,
+  parallelUploads: 1,
+  maxFilesize: 2,
+  addRemoveLinks: true,
+  autoProcessQueue: false,
+  headers: { 'X-CSRF-Token': csfrToken.value },
+  uploadMultiple: true,
+  accept: function(file, done) { done() },
+  init: function() {
+    const myDropzone = this;
 
-if (location.hash) {
+    console.log(myDropzone);
 
-  const hash = url.split("#");
-  let tabLink =hash[1];
+    userForm.addEventListener('submit', function(event) {
+      if(myDropzone.getQueuedFiles().length > 0){
+        event.preventDefault();
+        event.stopPropagation();
+        myDropzone.processQueue();
+      }
+    });
 
-  if (tabLink[tabLink.length - 1] === "/") {
-    tabLink = tabLink.substring(0, tabLink.length - 1)
-  }
+    myDropzone.on("sendingmultiple", function(file, xhr, formData) {
+      formData.append("authenticity_token", csfrToken.value);
+      formData = appendFormData(formData);
+    });
 
-  switchTab(document.querySelector('#myTab a[href="#'+tabLink+'"]').getAttribute("id"));
-  console.log(document.querySelector('#myTab a[href="#'+tabLink+'"]').getAttribute("id"));//.tab("show");
-  url = location.href.replace(/\/#/, "#");
-  history.replaceState(null, null, url);
-  setTimeout(() => {
-    window.scrollTo(0,0);
-  }, 400);
-}
+    myDropzone.on("complete", function(file) {
+      myDropzone.removeFile(file);
+    });
+  },
+  successmultiple: dropZoneOnSuccess
+};
 
-const tab = document.querySelector('a[data-toggle="tab"]')
+// const userInfoLink = document.getElementById("user-info-link")
+// const userInfoTab = document.getElementById("user-info-tab")
 
-// $(function() {
+// const bookingLink = document.getElementById("bookings-link")
+// const bookingTab = document.getElementById("bookings-tab")
 
-// document.addEventListener("DOMContentLoaded", function() {
-  Dropzone.options.filesField = {
-    url: (window.location.pathname),
-    method: 'put',
-    maxFiles: 1,
-    parallelUploads: 1,
-    maxFilesize: 2,
-    addRemoveLinks: true,
-    autoProcessQueue: false,
-    uploadMultiple: true,
-    accept: function(file, done) {
+// const trabisLink = document.getElementById("trabis-link")
+// const trabisTab = document.getElementById("trabis-tab")
 
-      done();
-    },
-    init: function() {
-      const myDropzone = this;
+// const dropZoneElement = document.getElementById("files-field");
+// const userForm = document.getElementById('user-form');
+// const inputFirstName = document.getElementById("input-first-name");
+// const inputLastName = document.getElementById("input-last-name");
+// const inputEmail = document.getElementById("input-email");
+// const inputPhone = document.getElementById("input-phone");
+// const inputStreet = document.getElementById("input-street");
+// const inputCity = document.getElementById("input-city");
+// const inputZip = document.getElementById("input-zip");
+// const inputCountry = document.getElementById("input-country");
 
-      userForm.addEventListener('submit', function(event) {
+// userInfoLink.classList.add('active', 'bg-white');
+// userInfoTab.style.display = 'block';
+// bookingTab.style.display = 'none';
+// trabisTab.style.display = 'none';
 
-        if(myDropzone.getQueuedFiles().length > 0){
-          event.preventDefault();
-          event.stopPropagation();
-          myDropzone.processQueue();
-        }
-        else
-        {
-          const formData = new FormData(userForm);
-          formData.append("first_name", inputFirstName.value);
-          formData.append("last_name", inputLastName.value);
-          formData.append("email", inputEmail.value);
-          formData.append("phone_number", inputPhone.value);
-          formData.append("street_address", inputStreet.value);
-          formData.append("city_address", inputCity.value);
-          formData.append("zip_address", inputZip.value);
-          formData.append("country_address", inputCountry.value);
+// const switchTab = (tab) => {
 
-          let response = fetch(window.location.pathname, {
-            method: 'PUT',
-            body: formData
-          });
+//   if (tab  === 'user-info-link') {
+//     userInfoTab.style.display = 'block';
+//     bookingTab.style.display = 'none';
+//     trabisTab.style.display = 'none';
 
-          alert("User info succesfuly updated");
+//     userInfoLink.classList.add('active', 'bg-white');
+//     bookingLink.classList.remove('active', 'bg-white');
+//     trabisLink.classList.remove('active', 'bg-white');
+//   } else if (tab  === 'bookings-link') {
+//     userInfoTab.style.display = 'none';
+//     bookingTab.style.display = 'block';
+//     trabisTab.style.display = 'none';
 
-        }
-      });
+//     userInfoLink.classList.remove('active','bg-white');
+//     bookingLink.classList.add('active', 'bg-white');
+//     trabisLink.classList.remove('active', 'bg-white');
+//   } else {
+//     userInfoTab.style.display = 'none';
+//     bookingTab.style.display = 'none';
+//     trabisTab.style.display = 'block';
 
-      myDropzone.on("sendingmultiple", function(file, xhr, formData) {
-        formData.append("first_name", inputFirstName.value);
-        formData.append("last_name", inputLastName.value);
-        formData.append("email", inputEmail.value);
-        formData.append("phone_number", inputPhone.value);
-        formData.append("street_address", inputStreet.value);
-        formData.append("city_address", inputCity.value);
-        formData.append("zip_address", inputZip.value);
-        formData.append("country_address", inputCountry.value);
-      });
+//     userInfoLink.classList.remove('active', 'bg-white');
+//     bookingLink.classList.remove('active', 'bg-white');
+//     trabisLink.classList.add('active', 'bg-white');
+//   }
 
-      myDropzone.on("complete", function(file) {
-        myDropzone.removeFile(file);
-      });
-    },
-    successmultiple: function(data,response) {
-      alert("User info succesfuly updated");
-      window.location.reload();
-    }
-  };
+// };
+
+// let url = location.href
+
+// const tabClicked = (event) => {
+//   let newUrl;
+
+//   switchTab(event.target.getAttribute("id"));
+//   const hash = event.target.getAttribute("href");
+//   let prefix = url.split("#")[0];
+
+//   if (prefix[prefix.length - 1] === "/") {
+//     prefix = prefix.substring(0, prefix.length - 1)
+//   }
+
+//   newUrl = prefix + hash;
+
+//   history.replaceState(null, null, newUrl);
+// };
+
+// userInfoLink.addEventListener("click", tabClicked);
+// bookingLink.addEventListener("click", tabClicked);
+// trabisLink.addEventListener("click", tabClicked);
+
+// if (location.hash) {
+
+//   const hash = url.split("#");
+//   let tabLink =hash[1];
+
+//   if (tabLink[tabLink.length - 1] === "/") {
+//     tabLink = tabLink.substring(0, tabLink.length - 1)
+//   }
+
+//   switchTab(document.querySelector('#myTab a[href="#'+tabLink+'"]').getAttribute("id"));
+//   console.log(document.querySelector('#myTab a[href="#'+tabLink+'"]').getAttribute("id"));//.tab("show");
+//   url = location.href.replace(/\/#/, "#");
+//   history.replaceState(null, null, url);
+//   setTimeout(() => {
+//     window.scrollTo(0,0);
+//   }, 400);
+// }
+
+// const tab = document.querySelector('a[data-toggle="tab"]')
+
+// // $(function() {
+
+// // document.addEventListener("DOMContentLoaded", function() {
+//   Dropzone.options.filesField = {
+//     url: (window.location.pathname),
+//     method: 'put',
+//     maxFiles: 1,
+//     parallelUploads: 1,
+//     maxFilesize: 2,
+//     addRemoveLinks: true,
+//     autoProcessQueue: false,
+//     uploadMultiple: true,
+//     accept: function(file, done) {
+
+//       done();
+//     },
+//     init: function() {
+//       const myDropzone = this;
+
+//       userForm.addEventListener('submit', function(event) {
+
+//         if(myDropzone.getQueuedFiles().length > 0){
+//           event.preventDefault();
+//           event.stopPropagation();
+//           myDropzone.processQueue();
+//         }
+//         else
+//         {
+//           const formData = new FormData(userForm);
+//           formData.append("first_name", inputFirstName.value);
+//           formData.append("last_name", inputLastName.value);
+//           formData.append("email", inputEmail.value);
+//           formData.append("phone_number", inputPhone.value);
+//           formData.append("street_address", inputStreet.value);
+//           formData.append("city_address", inputCity.value);
+//           formData.append("zip_address", inputZip.value);
+//           formData.append("country_address", inputCountry.value);
+
+//           let response = fetch(window.location.pathname, {
+//             method: 'PUT',
+//             body: formData
+//           });
+
+//           alert("User info succesfuly updated");
+
+//         }
+//       });
+
+//       myDropzone.on("sendingmultiple", function(file, xhr, formData) {
+//         formData.append("first_name", inputFirstName.value);
+//         formData.append("last_name", inputLastName.value);
+//         formData.append("email", inputEmail.value);
+//         formData.append("phone_number", inputPhone.value);
+//         formData.append("street_address", inputStreet.value);
+//         formData.append("city_address", inputCity.value);
+//         formData.append("zip_address", inputZip.value);
+//         formData.append("country_address", inputCountry.value);
+//       });
+
+//       myDropzone.on("complete", function(file) {
+//         myDropzone.removeFile(file);
+//       });
+//     },
+//     successmultiple: function(data,response) {
+//       alert("User info succesfuly updated");
+//       window.location.reload();
+//     }
+//   };
 // });
 
 
